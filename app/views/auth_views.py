a659d3e50b30025672c7faa2eedca408171d04d4
Sungheon Jeong <elon4856@gmail.com>
2021-02-24 00:26:50 +0900

    비밀번호 구현

--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -5,6 +5,8 @@ from werkzeug.utils import redirect
 from app import db
 from app.forms import UserCreateForm, UserLoginForm
 from app.models import User
+from app.forms import UserCreateForm, UserLoginForm, ForeignUserCreateForm
+
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -23,6 +25,23 @@ def signup():
         else:
             flash('이미 존재하는 사용자입니다.')
     return render_template('auth/signup.html', form=form)
+
+@bp.route('/foreign_signup/', methods=('GET', 'POST'))
+def foreign_signup():
+    form = ForeignUserCreateForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            user = User(username=form.username.data,
+                        password=generate_password_hash(form.password1.data),
+                        email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('main.index'))
+        else:
+            flash('이미 존재하는 사용자입니다.')
+    return render_template('auth/foreign_signup.html', form=form)
+
 
 @bp.route('/login/', methods=('GET', 'POST'))
 def login():
--- app/views/main_views.py
+++ app/views/main_views.py
@@ -11,7 +11,7 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/hello')
 def hello_pybo():
-    return 'Hello, Pybo!'
+    return 'Hello~!!'
 
 
 @bp.route('/')

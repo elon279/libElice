b76d32c90352184f8ba4c8aa35c5616d26b71489
Sungheon Jeong <elon4856@gmail.com>
2021-02-27 00:09:18 +0900

    db relation 활용

--- app/views/borrow_views.py
+++ app/views/borrow_views.py
@@ -8,30 +8,14 @@ bp = Blueprint("borrow", __name__, url_prefix="/borrow")
 
 @bp.route('/<int:user_id>')
 def borrowedBooks(user_id):
-    borrowed_books = []
-    books = []
     borrow_log = BorrowLog.query.filter(BorrowLog.user_id == user_id).all()
-    for each_log in borrow_log:
-        borrowed_books.append(each_log.book_id)
-
-    for book_id2 in borrowed_books:
-        books.append(Book.query.get(book_id2))
-
-    return render_template('borrow_return/borrow_log.html', books=books, borrow_log=borrow_log)
+    return render_template('borrow_return/borrow_log.html', borrow_log=borrow_log)
 
 
 @bp.route('/toReturn/<int:user_id>')
 def booksToReturn(user_id):
-    borrowed_books = []
-    books = []
     borrow_log = BorrowLog.query.filter(BorrowLog.user_id == user_id).all()
-    for each_log in borrow_log:
-        borrowed_books.append(each_log.book_id)
-
-    for book_id2 in borrowed_books:
-        books.append(Book.query.get(book_id2))
-
-    return render_template('borrow_return/return.html', books=books, borrow_log=borrow_log)
+    return render_template('borrow_return/return.html', borrow_log=borrow_log)
 
 
 @bp.route('/<int:user_id>/<int:book_id>', methods=('GET', 'POST'))

57df5d953d51a01a878f9b18726861ee3363a56c
Sungheon Jeong <elon4856@gmail.com>
2021-02-26 21:18:38 +0900

    함수명 변경 작업

--- app/views/rental_views.py
+++ app/views/rental_views.py
@@ -7,7 +7,7 @@ bp = Blueprint("rental", __name__, url_prefix="/rental")
 
 
 @bp.route('/<int:user_id>')
-def bookRent(user_id):
+def borrowedBooks(user_id):
     rental_books = []
     books = []
     rental_log = RentalLog.query.filter(RentalLog.user_id == user_id).all()
@@ -17,12 +17,11 @@ def bookRent(user_id):
     for book_id2 in rental_books:
         books.append(Book.query.get(book_id2))
 
-    return render_template('rentReturn/rentLog.html', books=books, rental_log= rental_log)
+    return render_template('rentReturn/rentLog.html', books=books, rental_log=rental_log)
 
 
-
-@bp.route('/return/<int:user_id>')
-def bookReturn(user_id):
+@bp.route('/toReturn/<int:user_id>')
+def booksToReturn(user_id):
     rental_books = []
     books = []
     rental_log = RentalLog.query.filter(RentalLog.user_id == user_id).all()
@@ -35,9 +34,8 @@ def bookReturn(user_id):
     return render_template('rentReturn/return.html', books=books, rental_log=rental_log)
 
 
-
-@bp.route('/return/<int:user_id>/<int:book_id>', methods=('GET', 'POST'))
-def returningBook(book_id, user_id):
+@bp.route('/<int:user_id>/<int:book_id>', methods=('GET', 'POST'))
+def returnBook(book_id, user_id):
     book = Book.query.filter(Book.id == book_id).first()
     book.stock = book.stock + 1
 
@@ -45,4 +43,4 @@ def returningBook(book_id, user_id):
     rental_log.due_date = datetime.now()
     db.session.commit()
 
-    return redirect(url_for('book.bookDetail', book_id = book.id))
+    return redirect(url_for('book.bookDetail', book_id=book.id))
